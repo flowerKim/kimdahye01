ef4e23eec95bae02bfc7ca21c3c399aba4ec29fb
kimdahye <aotlf_@nate.com>
2016-03-22 09:20:25 +0900

    .

--- src/main/java/org/zerock/service/BoardServiceImpl.java
+++ src/main/java/org/zerock/service/BoardServiceImpl.java
@@ -5,6 +5,8 @@ import java.util.List;
 import javax.inject.Inject;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Isolation;
+import org.springframework.transaction.annotation.Transactional;
 import org.zerock.domain.BoardVO;
 import org.zerock.domain.Criteria;
 import org.zerock.domain.SearchCriteria;
@@ -16,25 +18,78 @@ public class BoardServiceImpl implements BoardService {
   @Inject
   private BoardDAO dao;
 
+  
+  @Transactional
   @Override
   public void regist(BoardVO board) throws Exception {
+  
     dao.create(board);
+    
+    String[] files = board.getFiles();
+    
+    if(files == null) { return; } 
+    
+    for (String fileName : files) {
+      dao.addAttach(fileName);
+    }   
   }
+  
+  //
+//  @Override
+//  public void regist(BoardVO board) throws Exception {
+//    dao.create(board);
+//  }
 
+//  @Override
+//  public BoardVO read(Integer bno) throws Exception {
+//    return dao.read(bno);
+//  }
+
+
+  @Transactional(isolation=Isolation.READ_COMMITTED)
   @Override
   public BoardVO read(Integer bno) throws Exception {
+    dao.updateViewCnt(bno);
     return dao.read(bno);
   }
 
+  
+//  @Override
+//  public void modify(BoardVO board) throws Exception {
+//    dao.update(board);
+//  }
+  
+  @Transactional
   @Override
   public void modify(BoardVO board) throws Exception {
     dao.update(board);
+    
+    Integer bno = board.getBno();
+    
+    dao.deleteAttach(bno);
+    
+    String[] files = board.getFiles();
+    
+    if(files == null) { return; } 
+    
+    for (String fileName : files) {
+      dao.replaceAttach(fileName, bno);
+    }
   }
+  
 
+//  @Override
+//  public void remove(Integer bno) throws Exception {
+//    dao.delete(bno);
+//  }
+  
+  
+  @Transactional
   @Override
   public void remove(Integer bno) throws Exception {
+    dao.deleteAttach(bno);
     dao.delete(bno);
-  }
+  } 
 
   @Override
   public List<BoardVO> listAll() throws Exception {
@@ -64,5 +119,12 @@ public class BoardServiceImpl implements BoardService {
 
     return dao.listSearchCount(cri);
   }
+  
+
+  @Override
+  public List<String> getAttach(Integer bno) throws Exception {
+    
+    return dao.getAttach(bno);
+  }   
 
 }
